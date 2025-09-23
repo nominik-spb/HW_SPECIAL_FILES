21f35a4306bbd75ff37e3e8396a37df9f8453ebd
nominik <mike.nogai.spb@mail.ru>
2025-09-23 08:11:38 +0300

    change2

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -8,8 +8,7 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.*;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -21,7 +20,7 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
@@ -31,18 +30,38 @@ public class Main {
         List<Employee> list2 = parseXML("data.xml");
     }
 
-    private static List<Employee> parseXML(String fileName) {
+    private static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new File("company.xml"));
+        Document doc = builder.parse(new File("data2.xml"));
         Node root = doc.getDocumentElement();
+
         System.out.println( "Корневой элемент: " + root.getNodeName());
         read(root);
-        return List.of();
+        Element root = document.createElement("root");
+        document.appendChild(root);
+    }
+
+    private static void read(Node node) {
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                System. out.println( "Текущий узел: " + node_.getNodeName());
+                Element element = (Element) node_;
+                NamedNodeMap map = element.getAttributes();
+                for (int a = 0; a < map.getLength(); a++) {
+                    String attrName = map.item(a).getNodeName();
+                    String attrValue = map.item(a).getNodeValue();
+                    System. out.println( "Атрибут: " + attrName + "; значение: " + attrValue);
+                }
+                read(node_);
+            }
+        }
     }
 
     private static void writeString(String json, List<Employee> list) {
-        try(Writer writer = new FileWriter("writedata.csv")) {
+        try(Writer writer = new FileWriter("data2.csv")) {
             StatefulBeanToCsv<Employee> sbc =
                     new StatefulBeanToCsvBuilder<Employee>(writer)
                             .build();
